572194fd6898dc759215bd91bb661e664745653c
Riley Carter <riley@bad.pet>
2020-06-27 01:58:34 -0400

    server: Listen for requests to /

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ from main import webhook
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET", "POST"])
 @app.route("/<path:path>", methods=["GET", "POST"])
 def thing(*args, **kwargs):
     return webhook(request)
